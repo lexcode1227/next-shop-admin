1b0e003a4c1ff2227bd0dfdb75dc3cf1da06332b
Henry Agustin <henryagustin297@gmail.com>
2023-09-11 15:57:30 -0600

    componentes y rutas agregadas

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require("tailwindcss/colors");
+
+delete colors["lightBlue"];
+delete colors["warmGray"];
+delete colors["trueGray"];
+delete colors["coolGray"];
+delete colors["blueGray"];
+
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -7,10 +16,11 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        ...colors,
+        green: colors.emerald,
+        yellow: colors.amber,
+        purple: colors.violet,
       },
     },
   },
